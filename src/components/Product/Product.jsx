c8272536df708fab90024dd2bcfe02f608f0a75a
rajibrahman74e <rajiburrahman74@gmail.com>
2023-03-29 01:02:34 +0600

    [ADD]-overall-card-visible-in-UI

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import "./Product.css"
+import React from "react";
+import "./Product.css";
 
 const Product = (props) => {
-    console.log(props.product);
-    const {img, name, price, saller, rating} = props.product;
-    return (
-        <div className='product'>
-            <img src={img} alt="" />
-            <h6>{name}</h6>
-        </div>
-    );
+  console.log(props.product);
+  const { img, name, price, seller, ratings } = props.product;
+  return (
+    <div className="product">
+      <img src={img} alt="" />
+      <div className="product-info">
+        <h6>{name}</h6>
+        <p className="price-name">Price: {price}</p>
+        <p>Manufacturer: {seller}</p>
+        <p>Rating: {ratings}</p>
+      </div>
+      <button>Add to Cart</button>
+    </div>
+  );
 };
 
 export default Product;
