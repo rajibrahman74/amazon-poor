c69b3d17d28155bf3a8ee09d73fd432fd14abcc5
rajibrahman74e <rajiburrahman74@gmail.com>
2023-03-29 10:51:57 +0600

    [ADD]-fontAwesome-cdn

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import "./Product.css";
 
 const Product = (props) => {
@@ -14,7 +16,10 @@ const Product = (props) => {
         <p>Manufacturer: {seller}</p>
         <p>Rating: {ratings}</p>
       </div>
-      <button onClick={() => addToCart(props.product)}>Add to Cart</button>
+      <button onClick={() => addToCart(props.product)}>
+        Add to Cart
+        <FontAwesomeIcon icon={faShoppingCart} />
+        </button>
     </div>
   );
 };
