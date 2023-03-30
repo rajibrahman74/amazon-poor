b172c68414e231441f649ceefee5f8518469e6b5
rajibrahman74e <rajiburrahman74@gmail.com>
2023-03-30 12:54:28 +0600

    [ADD]-set-data-in-cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,9 +7,19 @@ const Cart = ({ cart }) => {
 
   let totalPrice = 0;
   let totalShipping = 0;
+  let quantity = 0;
   for (const product of cart) {
-    totalPrice = totalPrice + product.price;
-    totalShipping = totalShipping + product.shipping;
+    // solution one
+    // if (product.quantity === 0) {
+    //   product.quantity = 1
+    // } 
+
+    // solution two
+    product.quantity = product.quantity || 1;
+    
+    totalPrice = totalPrice + product.price * product.quantity;
+    totalShipping = totalShipping + product.shipping * product.shipping;
+    quantity = quantity + product.quantity;
   }
   const tax = (totalPrice * 7) / 100;
   const grandTotal = totalPrice + totalShipping + tax;
@@ -17,7 +27,7 @@ const Cart = ({ cart }) => {
   return (
     <div className="cart">
       <h5>Order Summary</h5>
-      <p>Selected items: {cart.length}</p>
+      <p>Selected items: {quantity}</p>
       <p>Total Price: ${totalPrice}</p>
       <p>Total Shipping Charge: ${totalShipping}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
