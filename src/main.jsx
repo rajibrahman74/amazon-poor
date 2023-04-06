48ef4c4475ceda169eab316fbf0c73b4b5045f5b
rajibrahman74e <rajiburrahman74@gmail.com>
2023-04-07 01:10:04 +0600

    [ADD]-orders-inventory-and-login-router-path

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Shop from "./components/Shop/Shop";
+import Home from "./components/Layout/Home";
+import Orders from "./components/Orders/Orders";
+import Inventory from "./components/Inventory/Inventory";
+import Login from "./components/Login/Login";
+import App from "./App";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Shop/>
-  }
-])
+    element: <Home />,
+    children: [
+      {
+        path: "/",
+        element: <Shop />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+      {
+        path: "inventory",
+        element: <Inventory />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
