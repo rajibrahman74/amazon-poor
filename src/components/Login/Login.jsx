4a20a576a55fce3ddc9a94f20f824cb072845e43
rajibrahman74e <rajiburrahman74@gmail.com>
2023-04-23 23:23:04 +0600

    [ADD]-complete-login-and-singup-components

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,47 @@
 import React from "react";
-import "./Login.css"
+import "./Login.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <h1>This is Login page</h1>
+    <div className="form-container">
+      <h2 className="form-title">Login</h2>
+      <div className="form-control">
+        <label htmlFor="">Email</label>
+        <input type="email" name="email" placeholder="Enter your email" />
+      </div>
+      <div className="form-control">
+        <label htmlFor="">Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+        />
+      </div>
+      <span>
+        <input className="login-button" type="submit" value="Login" />
+      </span>
+      <div className="toggle">
+        New to Ema-john?
+        <Link className="link" to="/singup">
+          Create New Account
+        </Link>
+      </div>
+      <div className="divider">
+        <hr className="divider-line" />
+        <span className="divider-text">or</span>
+        <hr className="divider-line" />
+      </div>
+      <div className="continue-with-google">
+        <img
+          src="https://o.remove.bg/downloads/f4fb3b91-5af5-469e-aab1-b6b9481488fd/2008px-Google__G__Logo.svg-removebg-preview.png"
+          alt=""
+        />
+        <span>Continue with Google</span>
+      </div>
     </div>
   );
 };
+
 
 export default Login;
--- src/components/SingUp/SingUp.jsx
+++ src/components/SingUp/SingUp.jsx
@@ -1,10 +1,52 @@
 import React from "react";
 import "./SingUp.css";
+import { Link } from "react-router-dom";
 
 const SingUp = () => {
   return (
-    <div>
-      <h2>This is SingUp page</h2>
+    <div className="form-container form-contaner-singup">
+      <h2 className="form-title">Sing Up</h2>
+      <div className="form-control">
+        <label htmlFor="">Email</label>
+        <input type="email" name="email" placeholder="Enter your email" />
+      </div>
+      <div className="form-control">
+        <label htmlFor="">Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+        />
+      </div>
+      <div className="form-control">
+        <label htmlFor="">Confirm Password</label>
+        <input
+          type="password"
+          name="confirm-password"
+          placeholder="Confirm your password"
+        />
+      </div>
+      <span>
+        <input className="login-button" type="submit" value="Sign Up" />
+      </span>
+      <div className="toggle">
+        Already have an account?
+        <Link className="link" to="/login">
+          Login
+        </Link>
+      </div>
+      <div className="divider">
+        <hr className="divider-line" />
+        <span className="divider-text">or</span>
+        <hr className="divider-line" />
+      </div>
+      <div className="continue-with-google">
+        <img
+          src="https://o.remove.bg/downloads/f4fb3b91-5af5-469e-aab1-b6b9481488fd/2008px-Google__G__Logo.svg-removebg-preview.png"
+          alt=""
+        />
+        <span>Continue with Google</span>
+      </div>
     </div>
   );
 };
